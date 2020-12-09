d46e695abf252e78306630fc499b1df0f2faae9e
Nicoletta Burduja <nicoburduja@Nicolettas-MBP.fritz.box>
2020-12-09 22:03:47 +0000

    hangman take input and remove lives if wrong

--- src/App.java
+++ src/App.java
@@ -1,22 +1,40 @@
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) {
-        //nbcbs!
-        StringBuffer strBuff=new StringBuffer();
-        String word="NCIS";
-        String merged;
-        char[] otherWord={'B','o','b','!'};
-        for(int i=0;i<=otherWord.length-1;i++){
-            if(word.charAt(i)!='I' && word.charAt(i)!='o'){
-                strBuff.append(word.charAt(i));
-            }
-
-
-            if(otherWord[i]!='I' && otherWord[i]!='o'){
-                strBuff.append(otherWord[i]);
-            }
-            
+        //vars
+        String[] gameWords={"january","february","march"};        
+        char userChar;
+        int lives=6;
+        boolean loseLife;
+        int randomPick=(int)(Math.random()*gameWords.length);
+        String playWord=gameWords[randomPick];
+        StringBuffer displayBuff=new StringBuffer();
+        String displayGuesses;        
+        for(int i=0;i<playWord.length();i++){
+            displayBuff.append("*");
         }
-        merged=strBuff.toString();
-        System.out.println(merged+"this is the word");
+        displayGuesses=displayBuff.toString();
+        //game logic
+        //input
+        do{
+            loseLife=true;
+            System.out.println("Hidden: "+displayGuesses);        
+            userChar=(JOptionPane.showInputDialog(null, "Enter a guess").charAt(0));
+            //checking guess against playWord
+            for(int i=0;i<playWord.length();i++){
+                if(playWord.charAt(i)==userChar){    
+                    //update buffer with correct chars             
+                    displayBuff.replace(i,i+1,userChar+"");
+                    loseLife=false;                    
+                }                                              
+            }
+            if(loseLife==true){
+                lives--;
+            }
+            displayGuesses=displayBuff.toString();      
+            System.out.println(displayGuesses);
+            System.out.println("Lives: "+lives);
+        }while(lives>0);
+        System.out.println("Game over");       
     }
 }
